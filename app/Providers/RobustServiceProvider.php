be06cec3e9e737c32a1d4fd34ce7c5b8192135c5
KanekiYuto <kaneki.yuto.404@gmail.com>
2024-11-23 15:26:16 +0800

    fix: null

---
+++ app/Providers/RobustServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * 服务提供者
+ *
+ * @author KanekiYuto
+ */
+class RobustServiceProvider extends ServiceProvider
+{
+
+    /**
+     * 注册服务
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * 引导服务
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+}
